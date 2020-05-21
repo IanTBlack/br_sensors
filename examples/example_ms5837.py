35b692b3c2b4d3b6a44c6d4c9970e820b594381a
Ian Black <blackia@oregonstate.edu>
2020-05-21 13:38:16 -0700

    Created examples folder.

--- examples/example_ms5837.py
+++ examples/example_ms5837.py
@@ -1,13 +1,19 @@
 """ Example
 Showcases functions from the ms5837 module.
+Utilizes the default resolution of 8192. 
+The temperature, pressure, depth, and altitude functions have a 
+"resolution" flag, which can be set to 256,512,1024,2048,4096, or 8192.
+
+The higher the value, the greater the data resolution. However, the cost is 
+an increase in conversion time. The difference between requesting 256 and 8192
+resolution data is about 20 ms.
 """
 import ms5837
 
 
 def main():    
     
-    resolution = 8192 #Resolution at which to get data in.
-    reference_atmosphere = 1013.25 
+    standard_atmosphere = 1013.25 
     
     #Set up pressure object.
     p = ms5837.MS5837()
@@ -19,10 +25,11 @@ def main():
     #Units options: Celsius,degC,C
     #         Fahrenheight,degF,F
     #         Kelvin,degK,K
-    temperature = p.temperature(units = 'degC',resolution = resolution)       
+    temperature = p.temperature(units = 'degC')       
     print(temperature)
     
-    #Request pressure from the sensor.
+    #Request pressure from the sensor. Use atmospheric pressure to get 
+    #   pressure exerted by water column.
     #Units options: millibar,mbar
     #         hectopascals,hPa
     #         decibar,dbar
@@ -32,20 +39,26 @@ def main():
     #         atmospheres,atm
     #         psi
     #         Torr, mmHg
-    pressure = p.pressure('millibar',reference_atmosphere,resolution)
+    pressure = p.pressure('millibar',standard_atmosphere)
     print(pressure)
     
     
-    #Request depth from the sensor.
+    #Request depth.
     #This is a standalone function and does not require the pressure function
     #   to be run separately. Pressure is required, so it is computed automatically.
     #Units options: meters,m
     #         feet,ft
     #         fathoms,ftm
     #Latitude is used in this calculation. By default it is 45.00000 deg N.
-    depth = p.depth('m',resolution = resolution, lat = 45.00000)
+    depth = p.depth(units = 'm' , lat = 45.00000)
     print(depth)
     
+    
+    #Request altitude.
+    #Units options: meters, m
+    #               feet, ft
+    altitude = p.altitude(units = 'm',sea_level_pressure = standard_atmosphere)
+    print(altitude)
     
     #Reset the sensor. Not necessary, but helps if there is a PROM error.
     p.reset_sensor()
